5a8e5016ecb22267754a589448d8e8ed4d9e051b
gayashan <gayashan.randimagamage@gmail.com>
2024-06-26 10:53:31 +0530

    add recovery code send functionality

--- main.py
+++ main.py
@@ -7,6 +7,8 @@ from pydantic import BaseModel
 from typing import List
 from bson import ObjectId
 import sib_api_v3_sdk
+from random import randint
+from datetime import datetime
 
 load_dotenv()
 
@@ -52,6 +54,19 @@ def all_users():
     for item in alll:
         item['_id'] = str(item['_id'])
     return alll
+
+# send recovery code mail
+def sendRecoveryCode(recoveryCode):
+    subject = "recovery code"
+    sender = {"name":"gamage.me","email":"gayashan.randimagamage@gmail.com"}
+    to = [{"email": 'chamodijanithya@gmail.com',"name": 'chomodi janithya'}]
+    headers = {"Some-Custom-Name":"unique-id-1234"}
+    # params = {"parameter":"My param value","subject":"New Subject"}
+    send_smtp_email = sib_api_v3_sdk.SendSmtpEmail(to=to, headers=headers, sender=sender, subject=subject, template_id=2, params={"code": recoveryCode})
+
+    api_response = api_instance.send_transac_email(send_smtp_email)
+    return api_response
+
 
 # check duplicate emails
 def checkMail(userEmail):
@@ -126,3 +141,34 @@ async def userRegister (userDetails : userDetails):
             'passwod' : userDetails.password
         })
         return 'succsessfull'
+
+@app.patch('/send-recoverycode/{email}', tags=['login'])
+async def sendRecoveryCode(email : str):
+    checkMails = checkMail(email)
+    if checkMails == False:
+        return 'email not found'
+    else:
+        b = users.find_one({'email' : email})
+        recoveryCode = randint(1000, 9999)
+        if 'recovery code' in b:
+            if len(b['recovery code']) < 3:
+                b['recovery code'].append({'code': recoveryCode, 'time' : datetime.now()})
+                users.update_one({'email' : email}, {'$set' : b})
+                sendRecoveryCode(recoveryCode)
+                return 'created new code'
+
+            elif len(b['recovery code']) == 3:
+                time_difference = int((datetime.now() - b['recovery code'][0]['time']).total_seconds()/60)
+                if time_difference < 5:
+                    return('you request too many times... try again laiter')
+                else:
+                    b['recovery code'].pop(0)
+                    b['recovery code'].append({'code': recoveryCode, 'time' : datetime.now()})
+                    users.update_one({'email' : email}, {'$set' : b})
+                    sendRecoveryCode(recoveryCode)
+                    return 'created new code'
+        else:
+            b['recovery code'] = [{'code': recoveryCode, 'time' : datetime.now()}]
+            users.update_one({'email' : email}, {'$set' : b})
+            sendRecoveryCode(recoveryCode)
+            return 'created new code'
